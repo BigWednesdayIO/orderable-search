8521d54d50a893f31ced48bcb5d75ff21ae87746
Chris Impey <chris@bigwednesday.io>
2015-12-11 16:50:53 +0000

    setup a hapi api with swagger and version route

---
+++ server/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Swaggered = require('hapi-swaggered');
+
+const plugins = [{
+  register: require('hapi-version-route')
+}, {
+  register: Swaggered,
+  options: {
+    auth: false,
+    info: {
+      title: 'Orderable Search API',
+      version: process.env.npm_package_version
+    }
+  }
+}];
+
+module.exports = callback => {
+  const server = new Hapi.Server();
+
+  server.connection({port: 8080});
+
+  server.register(plugins, err => {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, server);
+  });
+};
